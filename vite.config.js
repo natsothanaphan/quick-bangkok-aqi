5aa5a3530c53779453071862b259adf3b70f3277
Nat Sothanaphan <natsothanaphan@gmail.com>
2025-02-24 16:51:27 +0700

    draft backend modernize

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      // All requests starting with /api are proxied to the public URL.
       '/api': {
-        target: 'https://stations.airbkk.com',
+        target: 'http://127.0.0.1:5001/quick-bangkok-aqi/asia-southeast1/app',
         changeOrigin: true,
-        // Remove the "/api" prefix when forwarding the request
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  }
-})
+      },
+    },
+  },
+});
